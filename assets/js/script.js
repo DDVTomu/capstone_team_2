8d232dcad1115ac67146c77fca592e23b69b63da
DDVTomu <phan031099@gmail.com>
2023-04-20 20:23:49 +0700

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
-window.onscroll = function() {myFunction()};
+window.onscroll = function () {
+  myFunction();
+};
 
 var header = document.getElementById("sticky-header");
 var sticky = header.offsetTop;
 
 function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky-theme");
-  } else {
+  if (window.pageYOffset === 0) {
     header.classList.remove("sticky-theme");
+  } else {
+    header.classList.add("sticky-theme");
   }
 }
