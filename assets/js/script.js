9d60df40c9eeb8cc1fa0c510cbd48357070f316c
DDVTomu <phan031099@gmail.com>
2023-04-21 09:50:35 +0700

    merge source from Hung and Minh

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,21 @@ function myFunction() {
     header.classList.add("sticky-theme");
   }
 }
+
+var btnSwitchDark = document.getElementById("switch-dark");
+var btnSwitchLight = document.getElementById("switch-light");
+btnSwitchLight.style.display = "none";
+
+btnSwitchDark.onclick = function () {
+  document.getElementById("studious-body").classList.remove("light-mode");
+  document.getElementById("studious-body").classList.add("dark-mode");
+  btnSwitchDark.style.display = "none";
+  btnSwitchLight.style.display = "block";
+};
+
+btnSwitchLight.onclick = function () {
+  document.getElementById("studious-body").classList.remove("dark-mode");
+  document.getElementById("studious-body").classList.add("light-mode");
+  btnSwitchDark.style.display = "block";
+  btnSwitchLight.style.display = "none";
+};
